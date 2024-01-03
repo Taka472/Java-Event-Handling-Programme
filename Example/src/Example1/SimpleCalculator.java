0e9fca31cf8609211707f507e36ae717c0ae8e8c
Huy Trần Lê <tranlehuy0407@gmail.com>
2024-01-03 16:41:07 +0700

    adjust layout

--- Example/src/Example1/SimpleCalculator.java
+++ Example/src/Example1/SimpleCalculator.java
@@ -6,24 +6,28 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-public class SimpleCalculator extends JFrame implements ActionListener{
+public class SimpleCalculator extends JFrame implements ActionListener {
+	
 	private static final long serialVersionUID = 1L;
 	JTextField t1, t2, tr;
 	JButton b1, b2, b3;
 	JRadioButton r1, r2, r3, r4;
-	private int textFieldWidth = 20;
+	private int textFieldWidth = 1;
 	
 	@SuppressWarnings("unused")
 	public SimpleCalculator() {
 		super("Calculator");
 		JLabel l1, l2, result;
 		JPanel p = new JPanel();
+		//p.setLayout(new BorderLayout());
 		ButtonGroup btg = new ButtonGroup();
 		
 		
-		p.add(l1 = new JLabel("Số 1: ", JLabel.LEFT));
+		/*p.add(l1 = new JLabel("Số 1: ", JLabel.LEFT));
+		p.add(Box.createHorizontalStrut(12));
 		p.add(t1 = new JTextField(textFieldWidth));
 		p.add(l2 = new JLabel("Số 2: ", JLabel.LEFT));
+		p.add(Box.createHorizontalStrut(12));
 		p.add(t2 = new JTextField(textFieldWidth));
 		p.add(result = new JLabel("Kết quả: ", JLabel.LEFT));
 		p.add(tr = new JTextField(textFieldWidth));
@@ -33,7 +37,52 @@ public class SimpleCalculator extends JFrame implements ActionListener{
 		p.add(r4 = new JRadioButton("Chia"));
 		p.add(b1 = new JButton("Giải"));
 		p.add(b2 = new JButton("Xóa"));
-		p.add(b3 = new JButton("Thoát"));
+		p.add(b3 = new JButton("Thoát"));*/
+		
+		
+		Box box1 = Box.createVerticalBox();
+		Box box11 = Box.createVerticalBox();
+		Box box12 = Box.createVerticalBox();
+		Box box13 = Box.createHorizontalBox();
+		Box boxR = Box.createHorizontalBox();
+		Box boxB = Box.createHorizontalBox();
+		
+		box11.add(l1 = new JLabel("Số 1: "));
+		box11.add(Box.createVerticalStrut(10));
+		box11.add(l2 = new JLabel("Số 2: "));
+		box11.add(Box.createVerticalStrut(10));
+		box11.add(result = new JLabel("Kết quả: "));
+		
+		
+		box12.add(t1 = new JTextField(textFieldWidth));
+		box12.add(Box.createVerticalStrut(5));
+		box12.add(t2 = new JTextField(textFieldWidth));
+		box12.add(Box.createVerticalStrut(5));
+		box12.add(tr = new JTextField(textFieldWidth));
+		
+		boxR.add(r1 = new JRadioButton("Cộng", true));
+		boxR.add(r2 = new JRadioButton("Trừ"));
+		boxR.add(r3 = new JRadioButton("Nhân"));
+		boxR.add(r4 = new JRadioButton("Chia"));
+		
+		boxB.add(b1 = new JButton("Giải"));
+		boxB.add(Box.createHorizontalStrut(5));
+		boxB.add(b2 = new JButton("Xóa"));
+		boxB.add(Box.createHorizontalStrut(5));
+		boxB.add(b3 = new JButton("Thoát"));
+		
+		
+		box13.add(box11);
+		box13.add(Box.createHorizontalStrut(10));
+		box13.add(box12);
+		
+		box1.add(Box.createVerticalStrut(10));
+		box1.add(box13);
+		box1.add(Box.createVerticalStrut(10));
+		box1.add(boxR);
+		box1.add(Box.createVerticalStrut(10));
+		box1.add(boxB);
+		p.add(box1);
 		
 		
 		btg.add(r1);
